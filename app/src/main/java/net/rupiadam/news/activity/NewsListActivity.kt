408047938d82a769e4a4de8bc8dc12d7d47c7a3c
rupiadam <rupi.adam@gmail.com>
2018-11-04 16:20:40 +0700

    add observer to list news adapter

--- app/src/main/java/net/rupiadam/news/activity/NewsListActivity.kt
+++ app/src/main/java/net/rupiadam/news/activity/NewsListActivity.kt
@@ -6,10 +6,20 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_news_list.*
+import kotlinx.android.synthetic.main.content_news_list.*
 import net.rupiadam.news.R
+import net.rupiadam.news.adapter.NewsAdapter
+import net.rupiadam.news.room.NewsViewModel
 
 class NewsListActivity : AppCompatActivity() {
+
+    private lateinit var adapter: NewsAdapter
+    private val newsVM by lazy {
+        ViewModelProviders.of(this).get(NewsViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +29,14 @@ class NewsListActivity : AppCompatActivity() {
         fab.setOnClickListener {
             startActivity(Intent(this, CreateNewsActivity::class.java))
         }
+
+        adapter = NewsAdapter()
+        recycler.adapter = adapter
+
+        newsVM.getAllNews().observe(this, Observer {
+            listNews ->
+            adapter.setNews(listNews)
+        })
 
     }
 
